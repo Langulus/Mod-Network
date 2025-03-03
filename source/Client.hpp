f45a2ad8ced0cbd767c655cbb49eee62c0e2036d
Epixu <dimomarkov@gmail.com>
2025-03-03 11:59:02 +0200

    Missing include; Pair API changes

--- source/Client.hpp
+++ source/Client.hpp
@@ -7,6 +7,7 @@
 ///                                                                           
 #pragma once
 #include "Shared.hpp"
+#include <Langulus/Flow/Factory.hpp>
 #include <Langulus/Verbs/Create.hpp>
 
 
